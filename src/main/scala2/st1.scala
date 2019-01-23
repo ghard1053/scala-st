ee9faf6ef94d1a1285334304c8486dbcce314a10
Takao <sukisya70@gmail.com>
2019-01-23 23:58:07 +0900

    190123

---
+++ src/main/scala2/st1.scala
@@ -0,0 +1,28 @@
+
+println("Hello world!")
+
+val msg = "Hello world!"
+val masg3: String = "Hello world!"
+
+var greeting = "Hello world!"
+greeting = "Hello, world!"
+
+def max(x: Int, y: Int): Int = {
+  if (x > y) x
+  else y
+}
+def max2(x: Int, y: Int) = {
+  if (x > y) x
+  else y
+}
+max(3, 5)
+
+def greet() = println("Hello, world!")
+
+
+var i = 0
+while (i < args.length) {
+  println(args(i))
+  i += 1
+}
+
