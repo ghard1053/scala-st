85983bd8730b72657d900fe3d290c92f54dddb29
Takao <sukisya70@gmail.com>
2019-01-24 23:12:11 +0900

    190124

--- src/main/scala2/st1.scala
+++ src/main/scala2/st1.scala
@@ -1,3 +1,4 @@
+import java.math.BigInteger
 
 println("Hello world!")
 
@@ -25,4 +26,34 @@ while (i < args.length) {
   println(args(i))
   i += 1
 }
+
+args.foreach(arg => println(arg))
+args.foreach((arg: String) => println(arg))
+args.foreach(println)
+
+for (arg <- args)
+  println(arg)
+
+
+
+val big  = new BigInteger("12345")
+
+val greetStrings: Array[Strng] = new Array[String](3)
+
+for (i <- 0 to 2)
+  print(greetStrings(i))
+
+val numNames = Array("zero", "one", "two")
+val numNames2 = Array.apply("zero", "one", "two")
+
+val oneTwoThree = List(1, 2, 3)
+
+val oneTwo = List(1, 2)
+val threeFour = List(3, 4)
+val oneTwoThreeFour = oneTwo ::: threeFour
+
+val twoThree = List(2, 3)
+val oneTwoThree2 = 1 :: twoThree
+
+val oneTwoThree3 = 1 :: 2 :: 3 :: Nil
 
