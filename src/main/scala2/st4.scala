5910fe9315d5674b5a3395796eaef6a25f69736f
Takao <sukisya70@gmail.com>
2019-01-26 19:09:16 +0900

    190126

--- src/main/scala2/st4.scala
+++ src/main/scala2/st4.scala
@@ -1,4 +1,4 @@
-// 4
+// 4-6
 
 
 class ChecksumAccumulator {
@@ -33,3 +33,41 @@ object Summer {
       println(arg + ": " + calculate(arg))
   }
 }
+
+
+class Rational(n: Int, d: Int) {
+  require(d != 0)
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+  def this(n: Int) = this(n, 1)
+
+  def + (that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+  def + (i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+  def - (i: Int): Rational =
+    new Rational(numer - i * denom, denom)
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+  def * (i: Int): Rational =
+    new Rational(numer * i, dneom)
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
+
+  override def toString = numer + "/" + denom
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+}
+
+implicit def intToRational(x: Int) = new Rational(x)
