7ef80fd08ec0d2a3cc1bd693ff13538801c99c4b
Takao <sukisya70@gmail.com>
2019-01-29 23:58:28 +0900

    190129

--- src/main/scala2/st8.scala
+++ src/main/scala2/st8.scala
@@ -42,3 +42,9 @@ object LongLines {
 var increase = (x: Int) => x + 1
 increase(10)
 
+val someNumbers = List(-11, -10, -5, 0, 5, 10)
+someNumbers.foreach((x: Int) => println(x))
+someNumbers.filter((x: Int) => x > 0)
+
+someNumbers.filter((x) => x > 0)
+someNumbers.filter(x => x > 0)
