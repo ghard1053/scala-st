233c20ebd64843c1c41f41784381ab079344c5f6
Takao <sukisya70@gmail.com>
2019-01-30 23:57:45 +0900

    190130

--- src/main/scala2/st8.scala
+++ src/main/scala2/st8.scala
@@ -48,3 +48,29 @@ someNumbers.filter((x: Int) => x > 0)
 
 someNumbers.filter((x) => x > 0)
 someNumbers.filter(x => x > 0)
+
+someNumbers.filter(_ > 0)
+
+val f = (_: Int) + (_: Int)
+
+someNumbers.foreach(println _)
+someNumbers.foreach(println)
+
+val a = sum _
+
+val b = sum(1, _: Int, 3)
+
+
+var more = 1
+val addMore = (x: Int) => x + more
+addMore(10) // 11
+
+var sum = 0
+someNumbers.foreach(sum += _)
+
+def makeIncreaser(more: Int) = (x: Int) => x + more
+val inc1 = makeIncreaser(1)
+val inc9999 = makeIncreaser(9999)
+inc1(10)
+inc9999(10)
+
