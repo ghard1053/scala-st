70f0a0e2c246f21b82ebac958a99633676825625
Takao <sukisya70@gmail.com>
2019-02-07 23:58:31 +0900

    190207

---
+++ src/main/scala3/st1.scala
@@ -0,0 +1,50 @@
+
+
+def isAlphanumeric(str: String): Boolean = str.matches("a-z")
+//val isAlphamericF = isAlphanumeric _
+
+//val isAlphamericF = (str: String) => str.matches("a-z")
+
+val isAlphamericF = new Function1[String, Boolean] {
+  def apply(str: String) = str.matches("a-z")
+}
+
+val words = Seq("Scala", "2.12")
+val alphamericWords = words.filter(isAlphamericF)
+
+
+val weight = 120
+var message: String = null
+if (weight <= 100) message = "OK"
+else message = "over"
+
+val message2 = if (weight <= 100) {
+  "OK"
+} else {
+  "over"
+}
+
+val message3 = if (weight <= 100) "OK" else "over"
+
+val n = 5
+n match {
+  case 1 => println("gold")
+  case 2 => println("silver")
+  case 3 => println("bronze")
+  case other => println("no prize")
+}
+
+val maybeNum: Option[Int] = Some(123)
+val maybeNum2: Option[Int] = None
+
+val num: Int = maybeNum match {
+  case Some(num) if num < 0 => 0
+  case Some(num) => num
+  case None => 0
+}
+
+num match  {
+  case 1 | 2 | 3 => println("Less than 4")
+  case 4 => println("Equal to 4")
+  case other => println("Greater than 4")
+}
