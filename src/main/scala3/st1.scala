ade57799d34bf3ecbb9a0133c8306b7d1dd9f6b6
Takao <sukisya70@gmail.com>
2019-02-15 23:58:34 +0900

    190215

--- src/main/scala3/st1.scala
+++ src/main/scala3/st1.scala
@@ -152,3 +152,27 @@ trait Namable {
   val name: String
   def display(): Unit = println(name)
 }
+
+class Employee(val name: String) extends AnyRef with Namable
+
+val taro = new Employee("taro")
+taro.display()
+
+
+import scala.collection.mutable.Buffer
+trait Enumerable[A] {
+  def foreach[B](fun: A => B):Unit
+
+  final def map[B](f: A => B): List[B] = {
+    var members = Buffer.empty[A]
+    foreach {m =>
+      members += f(m)
+    }
+    members.toList
+  }
+
+  final def filter(p: A => Boolean): List[A] = {
+    val members = Buffer.empty[A]
+    
+  }
+}
