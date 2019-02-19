8b33180627bc089dfa3613528606a7a5ea3b374a
Takao <sukisya70@gmail.com>
2019-02-19 23:58:35 +0900

    190219

--- src/main/scala3/st3.scala
+++ src/main/scala3/st3.scala
@@ -14,4 +14,37 @@ val mutableSeq = scala.collection.mutable.Seq(1, 2, 3)
 mutableSeq(0) = 10
 immutableSeq(0) = 10 // error
 
-val seqq: Seq[String] = Seq("A", "B", "C")
+seq.apply(1)
+seq(1)
+seq.head
+seq.last
+
+val seqEmpty = Seq()
+seqEmpty.headOption // None or Some
+
+seq.tail
+seq.init
+
+val seq1 = Seq(1, 2)
+10 +: seq
+seq :+ 10
+
+val seq2 = Seq(3, 4, 5)
+seq1 ++ seq2
+
+val seq3: Seq[String] = Seq("A", "B", "C")
+seq3.take(2)
+seq3.takeRight(2)
+
+val seq4 = Seq(1, 2, 3, 4, 5, 1, 2)
+seq4.takeWhile(_ < 3) // Seq[Int] = List(1, 2)
+
+seq4.drop(2)
+seq4.dropRight(3)
+seq4.dropWhile(_ < 3)
+
+seq4.filter(_ > 2)
+
+Seq(5, 1, 4, 2).sorted
+Seq("abc", "bcd", "ab").sorted
+Seq(5, 1, 4, 2).reverse
